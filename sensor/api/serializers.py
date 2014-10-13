6b8ce48b79a36e7cd38bbd81042674f8427e19dd
Derek Lieu <derek@developmentseed.org>
2014-10-13 12:16:36 -0400

    Creating serializers, permissions files

---
+++ sensor/api/serializers.py
@@ -0,0 +1,11 @@
+from django.forms import widgets
+from rest_framework import serializers
+from sensors.models import Reading
+from django.contrib.auth.models import User
+
+class ReadingSerializer(serializers.ModelSerializer):
+    owner = serializers.Field(source='owner.username')
+    class Meta:
+        model = Reading
+        fields = ('created', 'owner', 'pm10', 'pm10_reading',
+                  'pm25', 'pm25_reading')
