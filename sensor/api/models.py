8fa8dd56a160a8cf6b94e6cf255e0cda6617371d
Derek Lieu <derek@developmentseed.org>
2014-10-13 11:56:45 -0400

    Model fields contain PM10/25 fields

--- sensor/api/models.py
+++ sensor/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+class Reading(models.Model):
+    # Authenticating on user
+    owner = models.ForeignKey('auth.User', related_name='sensors',
+                              default='')
+
+    # When the row gets made
+    created = models.DateTimeField(auto_now_add=True)
+
+    # Data on sensor readings
+    # TODO how do you programmatically set these fields?
+    pm10 = models.IntegerField()
+    pm10_reading = models.IntegerField()
+    pm25 = models.IntegerField()
+    pm25_reading = models.IntegerField()
+
+    class Meta:
+        ordering = ('-created',)
