b4712e75655108d396e5a4ee9b274b34c338e5b9
Marc Farra <marcfarra@gmail.com>
2014-12-17 03:54:32 -0500

    Include createHour field that will streamline groupBy operations in the db

--- api/models.py
+++ api/models.py
@@ -3,18 +3,22 @@ from django.utils.timezone import now
 
 class Reading(models.Model):
     # Authenticating on user
-    owner = models.ForeignKey('auth.User', related_name='api',
+    owner = models.ForeignKey('auth.User', related_name='readings',
                               default='')
 
     # When the row gets made
     created = models.DateTimeField(auto_now_add=True)
+    createdHour = models.DateTimeField(default = datetime.datetime.now, blank=True)
 
     # Data on sensor readings
     # TODO how do you programmatically set these fields?
-    pm10 = models.IntegerField()
-    pm10_reading = models.IntegerField()
-    pm25 = models.IntegerField()
-    pm25_reading = models.IntegerField()
+    pm10 = models.IntegerField(default = 0)
+    pm25 = models.IntegerField(default = 0)
+    pm10count = models.IntegerField(default = 0)
+    pm25count = models.IntegerField(default = 0)
 
     class Meta:
         ordering = ('-created',)
+
+    def save(self, *args, **kwargs):
+        super(Reading, self).save(*args, **kwargs)
