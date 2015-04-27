32da24303ed4cee8d51a577391c168683bda9b42
Scisco <scisco7@gmail.com>
2015-04-27 16:05:04 -0400

    New token email

--- sensor_rest_api/api/v1/mailer.py
+++ sensor_rest_api/api/v1/mailer.py
@@ -42,3 +42,39 @@ class VerificaitonEmail(object):
                                      [self.to_email])
         msg.attach_alternative(html_content, "text/html")
         msg.send()
+
+
+class TokenEmail(object):
+    def __init__(self, to_email, token, sensor_name=None):
+        self.to_email = to_email
+        self.token = token
+        self.sensor_name = sensor_name
+        self.from_email = settings.DEFAULT_FROM_EMAIL
+        self.site_url = settings.PORTAL_URL
+
+    def buildText(self):
+        text = 'Hi, \n\n'
+        text += 'The API Token is %s \n\n' % self.token
+        text += 'This sensor is registered using %s.\n\n' % self.to_email
+        text += 'Thank you!'
+
+        return text
+
+    def buildHTML(self):
+        text = '<p>Hi,</p>'
+        text += '<p>The API Token is <code>%s</code></p>' % self.token
+        text += '<p>This sensor is registered with %s.</p>' % self.to_email
+        text += '<p>Thank you!</p>'
+
+        return text
+
+    def send(self):
+        from_email = self.from_email
+        subject = 'API Token for sensor named %s' % self.sensor_name
+        text_content = self.buildText()
+        html_content = self.buildHTML()
+
+        msg = EmailMultiAlternatives(subject, text_content, from_email,
+                                     [self.to_email])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
