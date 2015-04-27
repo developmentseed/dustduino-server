d580d781b8fd169d186d3dc1ee439686879f997c
Scisco <scisco7@gmail.com>
2015-04-27 15:38:55 -0400

    update serializer with sensor model

--- sensor_rest_api/api/v1/serializers.py
+++ sensor_rest_api/api/v1/serializers.py
@@ -1,23 +1,20 @@
 from rest_framework import serializers
-from api.models import Reading
-from django.contrib.auth.models import User
 
-import datetime
+from sensors.models import Reading, Sensor
+
 
 class ReadingSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Reading
-        fields = ('url', 'pm10', 'pm25', 'pm10count', 'pm25count','created', 'owner','createdHour')
+        fields = ('url', 'pm10', 'pm25', 'pm10count', 'pm25count', 'created', 'owner', 'createdHour')
 
     def create(self, validated_data):
         return Reading.objects.create(**validated_data)
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    readings = serializers.HyperlinkedRelatedField(many=True, view_name='reading-detail', read_only=True)
 
-    class Meta: 
-        model = User
-        fields = ('url', 'username', 'password', 'email', 'readings')
-        write_only_fields = ('password',)
+class SensorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Sensor
