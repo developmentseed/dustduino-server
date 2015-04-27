2e3bc21f27b9f85c558a0e5a14881679ddf96f46
Scisco <scisco7@gmail.com>
2015-04-27 17:25:22 -0400

    Use the correct subject

--- sensor_rest_api/config/production.py
+++ sensor_rest_api/config/production.py
@@ -16,7 +16,7 @@ class Production(Base):
     EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix="", environ_name="SENDGRID_PASSWORD")
     EMAIL_HOST_USER = values.SecretValue(environ_prefix="", environ_name="SENDGRID_USERNAME")
     EMAIL_PORT = values.IntegerValue(587, environ_prefix="", environ_name="EMAIL_PORT")
-    EMAIL_SUBJECT_PREFIX = values.Value('[ad_brain] ', environ_name="EMAIL_SUBJECT_PREFIX")
+    EMAIL_SUBJECT_PREFIX = values.Value('[Sensor API] ', environ_name="EMAIL_SUBJECT_PREFIX")
     EMAIL_USE_TLS = True
     SERVER_EMAIL = EMAIL_HOST_USER
     # END EMAIL
