4651be94e8dbd8de4570eacc9461625f166936de
Scisco <scisco7@gmail.com>
2015-04-27 12:22:31 -0400

    change url endpoint to sensors

--- sensor_rest_api/api/v1/urls.py
+++ sensor_rest_api/api/v1/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
-from api.views import ReadingViewSet, UserViewSet
+from api.views import ReadingViewSet, SensorViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'readings', ReadingViewSet)
-router.register(r'devices', UserViewSet)
+router.register(r'sensors', SensorViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls))
