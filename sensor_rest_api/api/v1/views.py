358141aa79bc14df6d865bf564051542b464e3fd
Scisco <scisco7@gmail.com>
2015-04-27 12:25:09 -0400

    PEP8 clean up

--- sensor_rest_api/api/v1/views.py
+++ sensor_rest_api/api/v1/views.py
@@ -1,36 +1,33 @@
-from api.models import Reading
+import json
+import datetime
+from StringIO import StringIO
+
 from django.contrib.auth.models import User
 from django.db.models import Avg
-from api.serializers import UserSerializer, ReadingSerializer
-from rest_framework import status
+from django.core.serializers.json import DjangoJSONEncoder
 
 from rest_framework import permissions
-from api.permissions import IsOwnerOrReadOnly, IsUserOrReadOnly
-
-from rest_framework import renderers, viewsets
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import list_route
-
-import datetime
-
-from django.core.serializers.json import DjangoJSONEncoder
 from rest_framework.parsers import JSONParser
-from StringIO import StringIO
-import json 
+
+
+from api.models import Reading
+from api.serializers import UserSerializer, ReadingSerializer
+from api.permissions import IsOwnerOrReadOnly, IsUserOrReadOnly
 
 
 class ReadingViewSet(viewsets.ModelViewSet):
-    queryset = Reading.objects.all();
+    queryset = Reading.objects.all()
     serializer_class = ReadingSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                            IsOwnerOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
-    def perform_create(self, serializer):      
-        instance = serializer.save(owner = self.request.user)
-        hour = instance.created - datetime.timedelta(
-            seconds = 60*instance.created.minute + instance.created.second, 
-            microseconds = instance.created.microsecond)
-        serializer.save(createdHour = hour)
+    def perform_create(self, serializer):
+        instance = serializer.save(owner=self.request.user)
+        hour = instance.created - datetime.timedelta(seconds=60*instance.created.minute + instance.created.second,
+                                                     microseconds=instance.created.microsecond)
+        serializer.save(createdHour=hour)
 
     def get_queryset(self):
         queryset = Reading.objects.all()
@@ -42,7 +39,7 @@ class ReadingViewSet(viewsets.ModelViewSet):
         if created:
             date = [int(num) for num in created.split('-')]
             print date
-            date = datetime.datetime(date[0],date[1],date[2])
+            date = datetime.datetime(date[0], date[1], date[2])
             queryset = queryset.filter(created__gte=date)
         return queryset
 
@@ -50,13 +47,15 @@ class ReadingViewSet(viewsets.ModelViewSet):
     def aggregate(self, request):
         queryset = self.get_queryset()
         aggData = queryset.values('createdHour').order_by().annotate(
-            pm10=Avg('pm10'), 
-            pm25=Avg('pm25'), 
-            pm10count=Avg('pm10count'), 
-            pm25count = Avg('pm25count'))
+            pm10=Avg('pm10'),
+            pm25=Avg('pm25'),
+            pm10count=Avg('pm10count'),
+            pm25coun=Avg('pm25count')
+        )
 
         data = json.dumps(list(aggData), cls=DjangoJSONEncoder)
         return Response(JSONParser().parse(StringIO(data)))
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -64,11 +63,11 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (IsUserOrReadOnly,)
 
     def perform_create(self, serializer):
-        instance = serializer.save(password = self.request.DATA['password'])
+        instance = serializer.save(password=self.request.DATA['password'])
         instance.set_password(instance.password)
         instance.save()
 
     def perform_update(self, serializer):
-        instance = serializer.save(password = self.request.DATA['password'])
+        instance = serializer.save(password=self.request.DATA['password'])
         instance.set_password(instance.password)
         instance.save()
