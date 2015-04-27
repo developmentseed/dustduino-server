a8b5e5019962221e433f985b9a1fba9b8e9114a6
Scisco <scisco7@gmail.com>
2015-04-27 15:39:06 -0400

    replace user with sensor

--- sensor_rest_api/api/v1/urls.py
+++ sensor_rest_api/api/v1/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from api.views import ReadingViewSet, SensorViewSet
+from api.v1.views import ReadingViewSet, SensorViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
