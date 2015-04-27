e8344b9faa31e73537e7a9665567d79c0182d7cb
Scisco <scisco7@gmail.com>
2015-04-27 16:05:28 -0400

    better error messages

--- sensor_rest_api/sensors/views.py
+++ sensor_rest_api/sensors/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.db import transaction
 from rest_framework.authtoken.models import Token
 
-from sensors.models import SensorVerification
+from sensors.models import SensorVerification, Sensor
+from api.v1.mailer import TokenEmail
 
 
 def verify(request):
@@ -19,23 +20,34 @@ def verify(request):
             verify = SensorVerification.objects.get(verification_code=code)
 
             if verify.verified:
-                return render(request, 'sensors/error.html',
-                              {'message': 'This verification code is already used. If you look for the API token, check your email.'})
+                return render(
+                    request,
+                    'sensors/error.html',
+                    {
+                        'message': 'This verification code is already used. If you look for the API token, ' +
+                                   'check your email.'
+                    }
+                )
             else:
                 with transaction.atomic():
                     verify.verified = True
-                    verify.account.is_active = True
                     verify.save()
+                    # Activate user account
+                    verify.account.is_active = True
                     verify.account.save()
 
                     # Create token
                     token = Token.objects.get_or_create(user=verify.account)
 
-                    # Activate user account
-
                     # Send Token Email
+                    sensor = Sensor.objects.get(account=verify.account)
 
-                    return render(request, 'sensors/verify.html', {'verify': verify, 'token': token[0].key})
+                    mail = TokenEmail(verify.account.email, token[0].key, sensor.sensor_name)
+                    mail.send()
+
+                    return render(request, 'sensors/verify.html', {'verify': verify,
+                                                                   'token': token[0].key,
+                                                                   'sensor': sensor})
 
         except SensorVerification.DoesNotExist:
             return render(request, 'sensors/error.html',
