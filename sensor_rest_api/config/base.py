91e8f2cd8c65164bcd3c605ae39a52a1990e5519
Scisco <scisco7@gmail.com>
2015-04-28 11:21:49 -0400

    use token authentication

--- sensor_rest_api/config/base.py
+++ sensor_rest_api/config/base.py
@@ -108,3 +108,11 @@ class Base(Configuration):
         'PUT',
     )
     # End Django CORS
+
+    REST_FRAMEWORK = {
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+            'rest_framework.authentication.BasicAuthentication',
+            'rest_framework.authentication.SessionAuthentication',
+            'rest_framework.authentication.TokenAuthentication'
+        )
+    }
