0c59d794c5997ac84a13903c8a45f9e225243f81
Scisco <scisco7@gmail.com>
2015-04-28 16:15:16 -0400

    activate pagination

--- sensor_rest_api/config/base.py
+++ sensor_rest_api/config/base.py
@@ -127,5 +127,5 @@ class Base(Configuration):
             'rest_framework.authentication.TokenAuthentication'
         ),
         'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-        'PAGE_SIZE': 1
+        'PAGE_SIZE': 100
     }
